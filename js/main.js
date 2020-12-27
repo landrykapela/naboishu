c9d958eeb89e9bcec2fcaab6c4fb4787669b7ab2
Landry Kapela <landrykapela@gmail.com>
2020-12-27 14:15:01 +0300

    Add sections and navigation effect to menus

--- js/main.js
+++ js/main.js
@@ -6,3 +6,30 @@ if (dismissCookie) {
     parent.style.display = "none";
   });
 }
+
+//navigation menus
+const nav = document.getElementsByTagName("nav")[0];
+const nav_items = nav.children;
+// console.log("nav: ", nav_items);
+// var activeChild = "home";
+Array.from(nav_items).forEach((child) => {
+  if (child) {
+    // if (child.classList.contains("active")) activeChild = child.id;
+    // console.log("child: ", child);
+    child.addEventListener("click", (e) => {
+      let id = e.target.id;
+      let destination = "s-" + id;
+      Array.from(nav_items).forEach((c) => {
+        c.classList.remove("active");
+      });
+      child.classList.add("active");
+      navigateTo(destination);
+    });
+  }
+});
+
+const navigateTo = (destination) => {
+  let destinationView = document.getElementById(destination);
+  destinationView.scrollIntoView();
+  // document.getElementById(activeChild).classList.remove("active");
+};
