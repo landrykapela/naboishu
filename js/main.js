590184e765e75c8818ff44739207c3fd2445b457
Landry Kapela <landrykapela@gmail.com>
2020-12-28 13:25:05 +0300

    Add Travelers stories and footer

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@ if (dismissCookie) {
     parent.style.display = "none";
   });
 }
-
+//sections
+const sections = document.getElementsByTagName("section");
 //navigation menus
 const nav = document.getElementsByTagName("nav")[0];
 const nav_items = nav.children;
@@ -19,10 +20,7 @@ Array.from(nav_items).forEach((child) => {
     child.addEventListener("click", (e) => {
       let id = e.target.id;
       let destination = "s-" + id;
-      Array.from(nav_items).forEach((c) => {
-        c.classList.remove("active");
-      });
-      child.classList.add("active");
+      activateMenu(child);
       navigateTo(destination);
     });
   }
@@ -33,3 +31,48 @@ const navigateTo = (destination) => {
   destinationView.scrollIntoView();
   // document.getElementById(activeChild).classList.remove("active");
 };
+const activateMenu = (menu) => {
+  Array.from(nav_items).forEach((item) => {
+    item.classList.remove("active");
+  });
+  menu.classList.add("active");
+};
+const isInViewport = (element) => {
+  const rect = element.getBoundingClientRect();
+  let x = rect.left;
+  let x2 = x + element.offsetWidth;
+  let y = rect.top;
+  let y2 = y + element.offsetHeight;
+  return !(
+    x >= window.innerWidth ||
+    y >= window.innerHeight ||
+    x2 < 0 ||
+    y2 < 0
+  );
+  // return (
+  //   rect.top >= 0 &&
+  //   rect.left >= 0 &&
+  //   rect.bottom <=
+  //     (window.innerHeight || document.documentElement.clientHeight) &&
+  //   rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  // );
+};
+window.addEventListener("scroll", () => {
+  // console.log("scrolling..." + window.scrollY);
+
+  const top = document.querySelector("#top");
+  if (window.scrollY > 50) {
+    top.classList.add("dark-bg-trans");
+  } else {
+    top.classList.remove("dark-bg-trans");
+  }
+
+  Array.from(sections).forEach((section) => {
+    if (isInViewport(section)) {
+      let menuId = section.id.substring(2);
+      console.log("sections: ", menuId);
+      let menu = document.getElementById(menuId);
+      activateMenu(menu);
+    }
+  });
+});
